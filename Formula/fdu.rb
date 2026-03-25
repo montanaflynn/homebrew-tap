6e2d104f0bec56e3d67a6d9403242249bc91a675
Montana Flynn <montana949@gmail.com>
2026-03-25 17:25:07 -0500

    Update fdu to 0.2.0

--- Formula/fdu.rb
+++ Formula/fdu.rb
@@ -1,8 +1,8 @@
 class Fdu < Formula
   desc "Fast disk usage analyzer with a streaming TUI"
   homepage "https://github.com/montanaflynn/fdu"
-  url "https://github.com/montanaflynn/fdu/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "bd7d4c9d42095ee0b72d7bd54dcae0224323a89735e490c13fe1accd96edee49"
+  url "https://github.com/montanaflynn/fdu/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2951662a1fdd18fbdc1c96de8462d5f2a834ced910635706946644763f5cdeef"
   license "MIT"
 
   depends_on "rust" => :build
